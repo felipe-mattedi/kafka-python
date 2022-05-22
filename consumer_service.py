101e7e05af9c4c2d440b037026b6c7f0822c4f75
Felipe Mattedi <felipe.mattedi@terra.com.br>
2022-05-22 19:43:55 -0300

    generaliza consumo

--- consumer_service.py
+++ consumer_service.py
@@ -3,25 +3,31 @@ from kafka import KafkaConsumer
 import time
 
 class Consumer:
-    def __init__(self, topico, grupo):
+    def __init__(self, topico, grupo, classe):
         self.topico = topico
         self.grupo = grupo
+        self.classe = classe
         self.consumer = KafkaConsumer(
             bootstrap_servers=['localhost:9092'],
             max_poll_records = 1,
             group_id = self.grupo,
             enable_auto_commit = True )
         self.consumer.subscribe([self.topico])
+        self.classe.aviso_tela()
+        self.consome_mensagem()
+        self.classe.aviso_tela_fim()
+
+    def analisa_mensagem(self,message):
+        self.classe.processa_mensagem(message)
 
     def consome_mensagem(self):
-
         try:
             while True:
                 message = self.consumer.poll()
 
                 if message:
-                    self.consumer.close()
-                    return message
+                    self.analisa_mensagem(message)
+                    continue
 
                 else:
                     time.sleep(2)
@@ -30,12 +36,6 @@ class Consumer:
         except(KeyboardInterrupt):
             print('Fim de execução do consumidor: ' + self.grupo)
             self.consumer.close()
-            return False
-
-        if(self.consumer): 
-            print('Mensagem consumida em', mensagem.topic, 'Offset: ', mensagem.offset, 'Conteúdo: ', mensagem.value.decode('utf-8'))
-            print(mensagem)
-            return mensagem.value.decode('utf-8')
 
 
 
--- fraude.py
+++ fraude.py
@@ -1,36 +1,27 @@
 from consumer_service import Consumer
+from threading import Thread
 from kafka import TopicPartition
 import json
 import time
 
+class Fraudes():
 
-def aviso_tela():
-    print('---------------------')
-    print('Start serviço fraudes')
-    print('---------------------')
-
-def aviso_tela_fim():
-    print('---------------------')
-    print('Stop serviço fraudes')
-    print('---------------------')
-
-def roteiro_principal():
-    consumidor = Consumer('TOPICO_RECEPCAO_PRODUTO', 'GRUPO_FRAUDES')
-    mensagem_consumida = consumidor.consome_mensagem()
-    if(mensagem_consumida):
-        for chave,valor in mensagem_consumida.items():
-            print('Fraude sendo processada para código compra: ', valor[0].value.decode('utf-8'))
+    def processa_mensagem(self, mensagem):
+        for item in mensagem.values():
+            print('Fraude sendo processada para código compra: ', item[0].value.decode('utf-8'))
             time.sleep(3)
             print('Análise de fraude processada!')
-            return True 
-    else:
-        return False
 
-aviso_tela()
+    def aviso_tela(self):
+        print('---------------------')
+        print('Start serviço fraudes')
+        print('---------------------')
 
-servico_fraude_on = True
+    def aviso_tela_fim(self):
+        print('---------------------')
+        print('Stop serviço fraudes')
+        print('---------------------')
 
-while(servico_fraude_on):
-    servico_fraude_on = roteiro_principal()
+analise_fraude = Fraudes()
+Consumer('TOPICO_RECEPCAO_PRODUTO', 'GRUPO_FRAUDES', analise_fraude)
 
-aviso_tela_fim()
